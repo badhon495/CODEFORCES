4bc360235f2c5d224ca684a24cc1f6746c8dc35c
badhon <108542798+badhon496@users.noreply.github.com>
2022-09-15 17:14:36 +0600

    Update Queue at the School.py

--- Queue at the School.py
+++ Queue at the School.py
@@ -1,11 +1,6 @@
 a,b = map(int, input().split())
 u_input = input()
-for _ in range(b):
-    temp_input = u_input
-    for i in range(0,a+1,2):
-        if temp_input[i]=='B':
-            u_input = temp_input[i+1]+temp_input[i]
-        else:
-            u_input += temp_input[i]
+for i in range(b):
+    u_input = u_input.replace('BG','GB')
 
 print(u_input)
